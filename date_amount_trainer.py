faceba8a8617b4327b527f0017b3eba4320a9f9d
Subin Varghese <srvarghese95@gmail.com>
2023-01-28 23:35:05 -0600

    training loop with val and save

--- date_amount_trainer.py
+++ date_amount_trainer.py
@@ -1,4 +1,5 @@
 from datetime import date
+import enum
 from model.model import BellModel
 import torch
 import torch.optim as optim
@@ -16,11 +17,33 @@ model = BellModel().to(device)
 criterion = torch.nn.MSELoss()
 optimizer = optim.Adam(model.parameters(), lr=0.0001)
 
-min_loss = 1000000
+min_loss = 10000000000
+min_val_loss = 10000000000
 loss_list = []
 
-train_dataset = BillDataset(csv_file="Updated_Users.csv",root_dir=os.getcwd(),transform=True)
+train_dataset = BillDataset(csv_file="train.csv",root_dir=os.getcwd(),transform=True)
+val_dataset = BillDataset(csv_file="validation.csv",root_dir=os.getcwd(),transform=True)
 train_dataloader = DataLoader(train_dataset,batch_size=BATCH_SIZE,shuffle=True, pin_memory=True,drop_last=True)
+
+validation_dataloader = DataLoader(val_dataset, batch_size=2,shuffle=True, pin_memory=True,drop_last=True)
+
+def test_val_accuracy(model, dataloader):
+    val_loss = 0
+    samples = 0 
+    with torch.no_grad():
+        for batch, data in enumerate(dataloader):
+            samples +=2
+            img, gt = data
+            out = model(img.to(device))
+            #amount_date.to(device)
+            amount = torch.squeeze(out["Amount"].float().to(device))
+            out_date = torch.squeeze(out["Date"].float().to(device))
+            amount_gt = gt[:,0].to(device).float()
+            date_gt = gt[:,1].to(device).float()
+            loss = criterion(out_date.float(),date_gt) + criterion(amount.float(), amount_gt)
+            val_loss = val_loss + loss
+    return val_loss/samples
+
 itr = 0 
 for epoch in range(EPOCHS):
     print(f"epoch {epoch}")
@@ -32,9 +55,11 @@ for epoch in range(EPOCHS):
         #print(f"date and amount vector {amount_date.shape} ")
         out = model(img.to(device))
         #amount_date.to(device)
-        amount = out["Amount"].float().to(device)
-        out_date = out["Date"].float().to(device)
-        loss = criterion(out_date.float(),gt[:,1].to(device).float()) + criterion(amount.float(),gt[:,0].to(device).float()) 
+        amount = torch.squeeze(out["Amount"].float().to(device))
+        out_date = torch.squeeze(out["Date"].float().to(device))
+        amount_gt = gt[:,0].to(device).float()
+        date_gt = gt[:,1].to(device).float()
+        loss = criterion(out_date.float(),date_gt) + criterion(amount.float(), amount_gt) 
         loss.backward()
         optimizer.step()
         loss_list.append(loss.detach().cpu())
@@ -43,7 +68,13 @@ for epoch in range(EPOCHS):
         if loss < min_loss:
             print(f"min_loss at itr: {itr}, Loss {loss}")
             min_loss = loss
-            torch.save(model.state_dict(), "min_loss_model_itr.pth")
+            torch.save(model.state_dict(), "min_training_loss_model.pth")
+
+    val_loss = test_val_accuracy(model, validation_dataloader)
+    if val_loss < min_val_loss:
+        print(f"min validation loss at epoch: {epoch}, Loss {val_loss}")
+        torch.save(model.state_dict(), "min_val_loss_model.pth")
+        min_val_loss = val_loss
 
 
 # plt.plot(loss_list)
