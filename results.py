201c9f5382757f5dff629b7f6cca5b36341a6a52
georgeb20 <89757049+georgeb20@users.noreply.github.com>
2023-01-29 10:16:51 -0600

    Add files via upload

--- results.py
+++ results.py
@@ -5,8 +5,18 @@ from PIL import Image
 from torch.utils.data import DataLoader
 from BillDataset import BillDataset
 import os
+
 #given the # of days since 1970 and the amount, find the receipt name
 
+
+## assuming the data input is three lists of the number of days and amount...
+document_id = ['00034'] 
+number_of_days = [17923]
+amount = [37.1]
+id_dict = {}
+correct = 0
+
+#given the # of days since 1970 and the amount, find the receipt name
 
 device = torch.device('cuda') 
 model = BellModel().to(device)
@@ -21,30 +31,36 @@ for batch, data in enumerate(train_dataloader):
     #print(f"img data  {img.shape}")
     #print(f"date and amount vector {amount_date.shape} ")
     out = model(img.to(device))
+    
 dataframe = pd.read_csv("test_transactions.csv")
-amount = out['Amount'].detach().cpu()[0][0]
-number_of_days = out['Date'].detach().cpu()[0][0]
+amount = out['Amount'].detach().cpu()[0]
+number_of_days = out['Date'].detach().cpu()[0]
 
-print(amount)
-print(number_of_days)
-id = None
-for i in range(len(dataframe.index)):
-    if amount == dataframe['amount'][i] and number_of_days == dataframe['date'][i]:
-        id = i
-        break
-if id == None:
+
+for ind, val in enumerate(number_of_days):
+    id = None
     for i in range(len(dataframe.index)):
-        if number_of_days == dataframe['date'][i] and amount == 0:
+        if amount[ind] == dataframe['amount'][i] and number_of_days[ind] == dataframe['date'][i]:
             id = i
             break
-if id == None:
-    for i in range(len(dataframe.index)):
-        if amount == dataframe['amount'][i] and number_of_days == 0:
-            id = i
-            break
+    if id == None:
+        for i in range(len(dataframe.index)):
+            if number_of_days[ind] == dataframe['date'][i]: # the amount in the csv would be zero not the prediction
+                id = i
+                break
+    if id == None:
+        for i in range(len(dataframe.index)):
+            if amount == dataframe['amount'][i]: # may not want to force it to be 0 .. could just search for unique amount
+                id = i
+                break
+        
+    # check to see if the documentid and image id match... 
+    predict_doc_id = dataframe['documentid'][id]
+    id_dict[document_id[ind]] = predict_doc_id # dictionary of values 
+    
+    if document_id[ind] == predict_doc_id :
+        correct += 1   
 
-
-if id is None:
-    print("No transaction found")
-else:
-    print('The receipt id is',dataframe['documentid'][id])
+#---- Calculating accuracy 
+acc = correct/len(number_of_days)
+print('The accuracy on the test set is:', acc)
